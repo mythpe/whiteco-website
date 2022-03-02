0791c85ceb27e0870b14ddbec608c6610cc564ba
MyTh <mythpe@gmail.com>
2022-03-02 12:29:48 +0300

    .

---
+++ privacy-policy/index.php
@@ -0,0 +1,3 @@
+<?php
+
+header("Location: https://whiteco-app.com/%D8%B3%D9%8A%D8%A7%D8%B3%D8%A9-%D8%A7%D9%84%D8%AE%D8%B5%D9%88%D8%B5%D9%8A%D8%A9.html");
---
+++ terms/index.php
@@ -0,0 +1,3 @@
+<?php
+
+header('Location: https://whiteco-app.com/%D8%A7%D9%84%D8%B4%D8%B1%D9%88%D8%B7-%D9%88%D8%A7%D9%84%D8%A3%D8%AD%D9%83%D8%A7%D9%85.html');
